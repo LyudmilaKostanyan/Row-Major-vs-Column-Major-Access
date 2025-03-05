626fe1d7b12e8ab145c700b99c346e272e43c6c2
Lyudmila Kostanyan <lkostanyan@griddynamics.com>
2025-03-05 05:26:34 -0800

    Naming changes and add output function

--- main.cpp
+++ main.cpp
@@ -7,84 +7,67 @@
 using namespace std;
 using namespace std::chrono;
 
-void rowMajorAccess(int** arr, int row_size, int col_size) {
+void rowMajorAccess(int** matrix, int row_size, int col_size) {
     volatile size_t sum = 0;
     for (int i = 0; i < row_size; i++) {
         for (int j = 0; j < col_size; j++) {
-            sum += arr[i][j];
+            sum += matrix[i][j];
         }
     }
 }
 
-void columnMajorAccess(int** arr, int row_size, int col_size) {
+void columnMajorAccess(int** matrix, int row_size, int col_size) {
     volatile size_t sum = 0;
     for (int j = 0; j < col_size; j++) {
         for (int i = 0; i < row_size; i++) {
-            sum += arr[i][j];
+            sum += matrix[i][j];
         }
     }
 }
 
-int **parse_and_initialize(zen::cmd_args &args, int &row_size, int &col_size)
+int** parse_and_initialize(zen::cmd_args &args, int &row_size, int &col_size)
 {
-    int **arr;
+    int **matrix;
 
-    auto row_size_str = args.get_options("--row_size");
-    auto col_size_str = args.get_options("--col_size");
+    auto row_options = args.get_options("--row_size");
+    auto col_options = args.get_options("--col_size");
     
-    if (row_size_str.empty() && col_size_str.empty())
+    if (row_options.empty() && col_options.empty())
     {
-        std::cout << "Error: please write " << (row_size_str.empty() ? "--row_size" : "--col_size") << " parameter.";
-        return NULL;
+        std::cout << "Error: please write " << (row_options.empty() ? "--row_size" : "--col_size") << " parameter.";
+        return nullptr;
     }
 
-    row_size = row_size_str.size() ? std::atoi(row_size_str[0].c_str()) : std::atoi(col_size_str[0].c_str());
-    col_size = col_size_str.size() ? std::atoi(col_size_str[0].c_str()) : row_size;
+    row_size = row_options.size() ? std::atoi(row_options[0].c_str()) : std::atoi(col_options[0].c_str());
+    col_size = col_options.size() ? std::atoi(col_options[0].c_str()) : row_size;
 
     if (!row_size || !col_size)
     {
         std::cout << "Error: Row and column values must be greater than 0.";
-        return NULL;
+        return nullptr;
     }
     
-    arr = new int*[row_size];
+    matrix = new int*[row_size];
     for (int i = 0; i < row_size; i++) {
-        arr[i] = new int[col_size];
+        matrix[i] = new int[col_size];
     }
     
     for (int i = 0; i < row_size; i++) {
         for (int j = 0; j < col_size; j++) {
-            arr[i][j] = i + j;
+            matrix[i][j] = i + j;
         }
     }
-    return arr;
+    return matrix;
 }
 
-int main(int argc, char **argv) {
-    zen::cmd_args  args(argv, argc);
-    int row_size;
-    int col_size;
-    int **arr;
-    
-    arr = parse_and_initialize(args, row_size, col_size);
-    if (!arr)
-        return 1;
-
-    auto start_row = high_resolution_clock::now();
-    rowMajorAccess(arr, row_size, col_size);
-    auto end_row = high_resolution_clock::now();
-    auto duration_row = duration_cast<milliseconds>(end_row - start_row);
-    auto start_col = high_resolution_clock::now();
-    columnMajorAccess(arr, row_size, col_size);
-    auto end_col = high_resolution_clock::now();
-    auto duration_col = duration_cast<milliseconds>(end_col - start_col);
-
+void output_results(auto duration_row, auto duration_col, auto row_size, auto col_size)
+{
     double row_ms = duration_row.count();
     double col_ms = duration_col.count();
     double diff_ms = (duration_col.count() - duration_row.count());
     double speedup = col_ms / row_ms;
 
-    cout << "Array Size: " << row_size << " x " << col_size << endl;
+    cout << "Matrix Size: " << row_size << " x " << col_size << endl;
     cout << fixed << setprecision(2);
     cout << "-----------------------------------------------------------------" << endl;
     cout << left << setw(15) << "" 
@@ -100,11 +83,33 @@ int main(int argc, char **argv) {
          << setw(12) << speedup 
          << setw(17) << diff_ms << endl;
     cout << "-----------------------------------------------------------------";
+}
+
+int main(int argc, char **argv) {
+    zen::cmd_args  args(argv, argc);
+    int row_size;
+    int col_size;
+    int **matrix;
+    
+    matrix = parse_and_initialize(args, row_size, col_size);
+    if (!matrix)
+        return 1;
+
+    auto start_row = high_resolution_clock::now();
+    rowMajorAccess(matrix, row_size, col_size);
+    auto end_row = high_resolution_clock::now();
+    auto duration_row = duration_cast<milliseconds>(end_row - start_row);
+    auto start_col = high_resolution_clock::now();
+    columnMajorAccess(matrix, row_size, col_size);
+    auto end_col = high_resolution_clock::now();
+    auto duration_col = duration_cast<milliseconds>(end_col - start_col);
+
+    output_results(duration_row, duration_col, row_size, col_size);
 
     for (int i = 0; i < row_size; i++) {
-        delete[] arr[i];
+        delete[] matrix[i];
     }
-    delete[] arr;
+    delete[] matrix;
 
     return 0;
 }
